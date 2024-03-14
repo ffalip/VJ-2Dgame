35fd5fb2984ad04f59b7440290938f60e230ea10
martiPJ03 <marti.puerta03@gmail.com>
2024-03-14 13:19:06 +0100

    Animacio esquerra feta

--- Pang/src/Pang/02-Bubble/Player.cpp
+++ Pang/src/Pang/02-Bubble/Player.cpp
@@ -20,25 +20,29 @@ void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
 	bJumping = false;
 	spritesheet.loadFromFile("images/PlayerInGame.png", TEXTURE_PIXEL_FORMAT_RGBA);
-	sprite = Sprite::createSprite(glm::ivec2(16, 16), glm::vec2(0.1, 0.25), &spritesheet, &shaderProgram);
+	sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(0.1, 0.2), &spritesheet, &shaderProgram);
 	sprite->setNumberAnimations(4);
-	
-		sprite->setAnimationSpeed(STAND_LEFT, 8);
-		sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.f));
-		
-		sprite->setAnimationSpeed(STAND_RIGHT, 8);
-		sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.25f, 0.f));
-		
-		sprite->setAnimationSpeed(MOVE_LEFT, 8);
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.25f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
-		
-		sprite->setAnimationSpeed(MOVE_RIGHT, 8);
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.25f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.5f));
-		
+
+	sprite->setAnimationSpeed(STAND_LEFT, 8);
+	sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.8f));
+
+	sprite->setAnimationSpeed(STAND_RIGHT, 8);
+	sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.f, 0.f));
+
+	sprite->setAnimationSpeed(MOVE_LEFT, 8);
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.8f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.1f, 0.8f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.2f, 0.8f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.3f, 0.8f));
+	sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.4f, 0.8f));
+
+	sprite->setAnimationSpeed(MOVE_RIGHT, 8);
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.f, 0.f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.1f, 0.f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.2f, 0.f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.3f, 0.f));
+	sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.4f, 0.f));
+
 	sprite->changeAnimation(0);
 	tileMapDispl = tileMapPos;
 	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posPlayer.x), float(tileMapDispl.y + posPlayer.y)));
